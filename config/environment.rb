ebac7451d49f8719ab287b434ae359afead8a839
vanitanaidu <vanita112003@gmail.com>
2018-08-31 09:17:08 -0400

    Heroku config

--- config/environment.rb
+++ config/environment.rb
@@ -1,12 +1,44 @@
-ENV['SINATRA_ENV'] ||= "development"
+# ENV['SINATRA_ENV'] ||= "development"
+#
+# require 'bundler/setup'
+# Bundler.require(:default, ENV['SINATRA_ENV'])
+#
+# ActiveRecord::Base.establish_connection(
+#   :adapter => "sqlite3",
+#   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+# )
+#
+#
+#
+# require 'rack-flash'
+# require_all 'app'
+
+
+
 
 require 'bundler/setup'
-Bundler.require(:default, ENV['SINATRA_ENV'])
+Bundler.require
 
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-)
+configure :development do
+  ENV['SINATRA_ENV'] ||= "development"
 
-require 'rack-flash'
+  ActiveRecord::Base.establish_connection(
+    :adapter => "sqlite3",
+    :database => "db/christmas_shopper#{ENV['SINATRA_ENV']}.sqlite"
+  )
+end
+
+configure :production do
+  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+
+  ActiveRecord::Base.establish_connection(
+    :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host     => db.host,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'utf8'
+  )
+end
+
 require_all 'app'
